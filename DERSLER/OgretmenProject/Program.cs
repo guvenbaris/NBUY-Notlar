7ec1b01449120ec0ad77473202e6eb3afb92b6fe
Güven Barış ÇAKAN <gvnbrs54@gmail.com>
2021-12-11 10:07:26 +0300

    Update Program.cs

--- DERSLER/OgretmenProject/Program.cs
+++ DERSLER/OgretmenProject/Program.cs
@@ -16,29 +16,29 @@ namespace OgretmenProject
         static void Main(string[] args)
         {
             OgretmenDal _ogretmenDal = new OgretmenDal();
-            
-            // Add all ogretmen from json file
-            //List<Ogretmen> ogretmens = new List<Ogretmen>();
-            //JsonDataHelper jsonDataHelper = new JsonDataHelper();
-            //ogretmens = jsonDataHelper.ReadJsonFile();
-            //foreach (var ogretmen in ogretmens)
-            //{
-            //    if (_ogretmenDal.Add(ogretmen))
-            //    {
-            //        continue;
-            //    }
-            //}
 
-            // Show all teachers with json formatting and write to file
-            //string resultEmpty = "";
-            //foreach (var ogretmen in _ogretmenDal.GetAll())
-            //{
-            //    string json = JsonConvert.SerializeObject(ogretmen, Formatting.Indented);
-            //    resultEmpty += json + ",";
-            //    Console.WriteLine(json);
-            //}
-            //File.WriteAllText(@"D:\path.json", resultEmpty); // dosyaya yazdık hazırladığımız json dosyasını
-            //Console.WriteLine("Done!!!");
+            Add all ogretmen from json file
+            List<Ogretmen> ogretmens = new List<Ogretmen>();
+            JsonDataHelper jsonDataHelper = new JsonDataHelper();
+            ogretmens = jsonDataHelper.ReadJsonFile();
+            foreach (var ogretmen in ogretmens)
+            {
+                if (_ogretmenDal.Add(ogretmen))
+                {
+                    continue;
+                }
+            }
+
+            Show all teachers with json formatting and write to file
+            string resultEmpty = "";
+            foreach (var ogretmen in _ogretmenDal.GetAll())
+            {
+                string json = JsonConvert.SerializeObject(ogretmen, Formatting.Indented);
+                resultEmpty += json + ",";
+                Console.WriteLine(json);
+            }
+            File.WriteAllText(@"D:\path.json", resultEmpty); // dosyaya yazdık hazırladığımız json dosyasını
+            Console.WriteLine("Done!!!");
 
 
             Console.WriteLine("Done");
