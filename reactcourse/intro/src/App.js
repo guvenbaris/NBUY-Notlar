34c3a11e5ab4788177d21e1b16312d27151400bf
Güven Barış ÇAKAN <gvnbrs54@gmail.com>
2021-12-12 19:51:08 +0300

    react-strap

--- reactcourse/intro/src/App.js
+++ reactcourse/intro/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
+import {Container,Row,Col} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+        <Row>
+          <Navi></Navi>
+        </Row>
+        <Container>
+        <Row>
+          <Col xs="3">
+            <CategoryList />
+          </Col>
+          <Col xs="9">
+            <ProductList />
+          </Col>
+        </Row>
+        </Container>
+
+
     </div>
   );
 }
---
+++ reactcourse/intro/src/Navi.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navi extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Navigation Component</h1>
+            </div>
+        );
+    }
+}
+
+export default Navi;
