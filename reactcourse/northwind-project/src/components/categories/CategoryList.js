8234038e19d31c4a87377961a37563fb4f6dabae
Güven Barış ÇAKAN <gvnbrs54@gmail.com>
2021-12-21 19:53:33 +0300

    northwind-project redux-thunk

--- reactcourse/northwind-project/src/components/categories/CategoryList.js
+++ reactcourse/northwind-project/src/components/categories/CategoryList.js
@@ -1,19 +1,53 @@
-import React, { Component } from 'react';
-import {connect} from "react-redux"
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { ListGroup, ListGroupItem,Badge } from "reactstrap";
+import { bindActionCreators } from "redux";
+import * as categoryAction from "../../redux/actions/categoryAction";
 
 class CategoryList extends Component {
-    render() {
-        return (
-            <div>
-            <h5>Seçili kategori : {this.props.currentCategory.categoryName}</h5>
-            </div>
-        );
-    }
+  componentDidMount() {
+    this.props.actions.getCategories();
+  }
+  selectCategory = (category) => {
+    this.props.actions.changeCategory(category);
+  };
+
+  render() {
+    return (
+      <div>
+        <Badge color="warning">Categories</Badge>
+        <ListGroup>
+          {this.props.categories.map((category) => (
+            <ListGroupItem
+              key={category.id}
+              onClick={() => this.selectCategory(category)}
+              active={category.id === this.props.currentCategory.id}
+            >
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+      </div>
+    );
+  }
 }
-function mapStateToProps(state){
-    return {
-        currentCategory:state.changeCategoryReducer
-    }
+function mapStateToProps(state) {
+  return {
+    currentCategory: state.changeCategoryReducer,
+    categories: state.categoryListReducer,
+  };
 }
 
-export default connect(mapStateToProps)(CategoryList);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      getCategories: bindActionCreators(categoryAction.getCategories, dispatch),
+      changeCategory: bindActionCreators(
+        categoryAction.changeCategory,
+        dispatch
+      ),
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList);
--- reactcourse/northwind-project/src/components/products/ProductList.js
+++ reactcourse/northwind-project/src/components/products/ProductList.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-
+import {connect} from 'react-redux'
+import { Badge } from 'reactstrap';
 class ProductList extends Component {
     render() {
         return (
             <div>
-                <h3>Products</h3>
+                <Badge color='warning'>
+                    Products
+                </Badge>
+
+                <Badge color='success'>
+                    {this.props.currentCategory.categoryName}
+                </Badge>
+
             </div>
         );
     }
 }
 
-export default ProductList;     
+function mapStateToProps(state) {
+    return {
+      currentCategory: state.changeCategoryReducer,
+    };
+  }
+
+export default connect(mapStateToProps)(ProductList) 
