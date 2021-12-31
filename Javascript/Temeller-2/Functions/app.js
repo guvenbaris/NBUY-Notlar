dce80730a2c28f717babeadd8bd3392c34740510
Güven Barış ÇAKAN <gvnbrs54@gmail.com>
2021-12-31 22:57:17 +0300

    Temeller-2 Created

---
+++ Javascript/Temeller-2/Functions/app.js
@@ -0,0 +1,66 @@
+//Fonksion Tanımlama 
+
+// function merhaba(name="Bilgi yok",age="Bilgi yok"){ //default params
+//     // if(typeof name === "undefined") name ="Bilgi Yok";
+//     // if(typeof age === "undefined") age ="Bilgi Yok";
+
+//     console.log(`İsim ${name} Yaş : ${age}`)
+// }
+
+// merhaba("Murat",25); // Function call
+
+
+// function square(x) {
+//     console.log(x*x);
+//     return x*x;
+// }
+
+// function cube(x){
+//     console.log(x*x*x);
+//     return x*x*x;
+// }
+
+// let a = cube(square(12));
+
+// console.log(a);
+
+
+// function merhaba(){
+//     return "Merhaba"
+// }
+// console.log(merhaba())
+
+//Funciton Expression
+// const merhaba = function(name){
+//     console.log("Merhaba" + name);
+// }
+// merhaba();
+
+//Immediately Invoked Function Expression (IIFE)
+// tanımlandığı yerde çalışan function türüdür.
+// Methodun alacağı değerleri en son da parantez içerisinde
+//veriyoruz
+// (function(name){
+//     console.log("Merhaba : "+name);
+// })("Murat");
+
+
+
+//Objectlerimiz ile function kullanımı
+const database = {
+    host:"localhost",
+    add:function(){
+        console.log("Eklendi")
+    },
+    get:function(){
+        console.log("Elde edildi")
+    },
+    update:function(id){
+        console.log(`Id : ${id} Güncellendi`);
+    },
+    delete:function(id){
+        console.log(`Id : ${id} Silindi`);
+    },
+}
+console.log(database.host);
+database.add();
---
+++ Javascript/Temeller-2/comparisonOperators/app.js
@@ -0,0 +1,100 @@
+// ==
+// ===
+// !=
+// !==
+// >
+// <
+// >=
+// <=
+
+// console.log(2==2);
+// console.log("js" == "java");
+
+// console.log(2=="2"); 
+// console.log(2 ==="2");
+
+// console.log(4>2);
+// console.log(2>4);
+
+// console.log(2 != 2);
+
+
+// console.log(4>=2);
+// console.log(2>=4);
+
+// //Mantıksal Bağlaçlar 
+// //Not Operatörü
+
+// console.log(!(2 != 2));
+
+// //And Operatörü 
+// console.log( (2==2)&& ("Ahmet" == "Ahmet"));
+
+// //Or Operatörü
+// console.log((4==2)|| ("Ahmet"=="Ahmet"));
+
+
+// const  error = true;
+
+// if(error){
+//     console.log("Hata oluştu...");
+// }
+// else{
+//     console.log("Hata oluşmadı...")
+// }
+
+// const user = "mmc";
+
+// if(user === "mmc"){
+//     console.log("Kullanıcı bulundu...");
+// }
+
+// else{
+//     console.log("Kullanıcı bulunmadı...")
+// }
+
+// const process = "1";
+
+// if(process == "1"){
+//     console.log("İşlem 1");
+// }
+// else if(process == "2"){
+//     console.log("İşlem 2");
+// }
+// else{
+//     console.log("Geçersiz işlem")
+// }
+
+// const number =100;
+
+// if(number === 100){
+//     console.log("Sayı 100'e eşit");
+// }
+// else{
+//     console.log("Sayı 100'e Eşit değil");
+// }
+
+// // Ternary Operator 
+
+// console.log(number === 100) ? "Sayı 100":"Sayı 100 Değil"
+
+// if(number === 100) console.log("Sayı 100");
+// else console.log("Sayı 100 değil...")
+
+// Switch Case
+
+const process  =2;
+
+switch(process){
+    case 1:
+        console.log("İşlem 1");
+        break;
+    case 2: 
+        console.log ("İşlem 2");
+        break;
+    case 3:
+        console.log("İşlem 3");
+        break;
+    default:
+        console.log("Geçersiz İşlem");
+}
