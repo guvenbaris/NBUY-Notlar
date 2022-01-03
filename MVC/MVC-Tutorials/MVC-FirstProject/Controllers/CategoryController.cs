ee18ceaba85b517b1d9a9641bdb58a1137f0adaa
Güven Barış ÇAKAN <gvnbrs54@gmail.com>
2022-01-03 14:48:25 +0300

    Mvc Project Started

---
+++ MVC/MVC-Tutorials/MVC-FirstProject/Controllers/CategoryController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_FirstProject.Models;
+
+
+namespace MVC_FirstProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+
+        private static List<Categories> _categoriesList = new List<Categories>();
+
+        //List<string> categories = new List<string>
+        //{
+        //    "Car","Computer","Tech"
+        //};
+
+
+        public ActionResult List()
+        {
+            return View(_categoriesList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //[HttpGet]
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]
+        public ActionResult Create(Categories category)
+        {
+            _categoriesList.Add(category);
+            return RedirectToAction("List");
+        }
+
+    }
+}
---
+++ MVC/MVC-Tutorials/MVC-FirstProject/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_FirstProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Create()
+        {
+            ViewBag.ProductName = "Laptop";
+            ViewBag.UnitPrice = 100;
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            ViewBag.ProductId = 1;
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            ViewBag.ProductId = 1;
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            ViewBag.ProductName = "Mouse";
+            ViewBag.UnitPrice = 20;
+            return View();
+        }
+    }
+}
