45c5d8dc20ce1829a6fe3ebcf55d9c5366a8387f
Güven Barış ÇAKAN <gvnbrs54@gmail.com>
2022-01-05 19:15:04 +0300

    MVC Working

---
+++ MVC/MVC-Tutorials/MVC-School/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_School.Models;
+
+namespace MVC_School.Controllers
+{
+    public class StudentController : Controller
+    {
+        //Veritabanı yerine List kullandık
+        public List<Student> Students = new List<Student>();
+
+        private Student student = new Student
+        {
+            EducationClass = "10/A",
+            Name = "Barış",
+            Number = 442,
+            Surname = "ÇAKAN"
+        };
+
+        
+
+
+        // GET: Student listesini yazdırsın. Yanlarında Düzenleme,Silme bağlantıları ile birlikte.
+        public ActionResult Index()
+        {
+            Students.Add(student);
+            return View(Students);
+        }
+
+
+    }
+}
---
+++ MVC/MVC-Tutorials/MVC-School/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_School.Models
+{
+    public class Student
+    {
+        public int  Number { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string EducationClass  { get; set; }
+
+    }
+}
